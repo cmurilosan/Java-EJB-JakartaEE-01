1b8f94557f9ad8d2ae3bd9ee9976be3702042d40
Cassio Murilo <cmurilo.san@gmail.com>
2021-02-15 18:28:29 -0300

    Aula 06 - Criando Producer

--- src/main/java/br/com/alura/job/AgendamentoEmailJob.java
+++ src/main/java/br/com/alura/job/AgendamentoEmailJob.java
@@ -3,9 +3,13 @@ package br.com.alura.job;
 import br.com.alura.entidade.AgendamentoEmail;
 import br.com.alura.servico.AgendamentoEmailServico;
 
+import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 import java.util.List;
 
 @Singleton
@@ -14,12 +18,19 @@ public class AgendamentoEmailJob {
     @Inject
     private AgendamentoEmailServico agendamentoEmailServico;
 
+    @Inject
+    @JMSConnectionFactory("java:jboss/DefaultJMSConnectionFactory")
+    private JMSContext context;
+
+    @Resource(mappedName = "java:/jms/queue/EmailQueue")
+    private Queue queue;
+
     @Schedule(hour = "*", minute = "*", second = "*/10")
     public void enviarEmail() {
         List<AgendamentoEmail> listarPorNaoAgendado =
                 agendamentoEmailServico.listarPorNaoAgendado();
         listarPorNaoAgendado.forEach(emailNaoAgendado -> {
-            agendamentoEmailServico.enviar(emailNaoAgendado);
+            context.createProducer().send(queue, emailNaoAgendado);
             agendamentoEmailServico.alterar(emailNaoAgendado);
         });
 //        SCHEDULE CONTROLA DE QUANTO EM QUANTO TEMPO SERA ENVIADO O EMAIL
